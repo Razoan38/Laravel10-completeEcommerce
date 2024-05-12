8fda32427b9310d234f50225c9182265472b5a56
S.M. Razoan <razoan4axiz@gmail.com>
2024-05-12 16:59:01 +0600

    order search with order list

--- app/Models/order.php
+++ app/Models/order.php
@@ -2,22 +2,95 @@
 
 namespace App\Models;
 
+use Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class order extends Model
 {
     use HasFactory;
+
     protected $table='orders';
 
     static public function getSingle($id)
     {
         return self::find($id);
     }
+
+    // static public function getRecord()
+    // {
+    //     $return = order::select('orders.*')
+    //            ->where('is_payment', '=', 1)
+    //            ->where('is_delete', '=', 0)
+    //            ->orderBy('id', 'desc')
+    //            ->paginate(30);
+
+    //            return $return;
+    // }
+
+
     static public function getRecord()
     {
-        $return = order::select('orders.*')
-               ->where('is_payment', '=', 1)
+        $return = order::select('orders.*');
+
+              if(!empty(Request::get('id'))) 
+              {
+                $return = $return->where('id', '=', Request::get('id'));
+              }
+
+              if(!empty(Request::get('first_name'))) 
+              {
+                $return = $return->where('first_name', 'like', '%'.Request::get('first_name').'%' );
+              }
+
+              if(!empty(Request::get('last_name'))) 
+              {
+                $return = $return->where('last_name', 'like', '%'.Request::get('last_name').'%' );
+              }
+
+              if(!empty(Request::get('email'))) 
+              {
+                $return = $return->where('email', 'like', '%'.Request::get('email').'%' );
+              }
+              if(!empty(Request::get('phone'))) 
+              {
+                $return = $return->where('phone', 'like', '%'.Request::get('phone').'%' );
+              }
+              if(!empty(Request::get('country'))) 
+              {
+                $return = $return->where('country', 'like', '%'.Request::get('country').'%' );
+              }
+
+              if(!empty(Request::get('state'))) 
+              {
+                $return = $return->where('state', 'like', '%'.Request::get('state').'%' );
+              }
+              
+              if(!empty(Request::get('city'))) 
+              {
+                $return = $return->where('city', 'like', '%'.Request::get('city').'%' );
+              }
+
+              if(!empty(Request::get('discount_code'))) 
+              {
+                $return = $return->where('discount_code', 'like', '%'.Request::get('discount_code').'%' );
+              }
+
+              if(!empty(Request::get('payment_method'))) 
+              {
+                $return = $return->where('payment_method', 'like', '%'.Request::get('payment_method').'%' );
+              }
+
+              if(!empty(Request::get('form_date'))) 
+              {
+                $return = $return->where('created_at', '>=', Request::get('form_date') );
+              }
+              if(!empty(Request::get('to_date'))) 
+              {
+                $return = $return->where('created_at', '<=', Request::get('to_date'));
+              }
+        
+        $return = $return->where('is_payment', '=', 1)
                ->where('is_delete', '=', 0)
                ->orderBy('id', 'desc')
                ->paginate(30);
--- resources/views/admin/order/list.blade.php
+++ resources/views/admin/order/list.blade.php
@@ -12,7 +12,7 @@ Order Page
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1>Order  List </h1>
+              <h1>Order List (Total : {{ $getRecord->total() }}) </h1>
             </div>
             {{-- <div class="col-sm-6" style="text-align: right">
               <a href="{{ route('category.add') }}" class="btn btn-primary">Add New Category </a>
@@ -29,10 +29,113 @@ Order Page
             <div class="col-md-12">
               @include('admin.layouts._message')
      
+          {{-- Search form start --}}
+              <form action="" method="GET">
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Order Search</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                    <div class="row">
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">ID</label>
+                              <input type="text" name="id" class="form-control" value="{{ Request::get('id') }}" placeholder="ID">
+                          </div>
+                       </div>
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">First Name</label>
+                              <input type="text" name="first_name" class="form-control" value="{{ Request::get('first_name') }}" placeholder="First Name">
+                          </div>
+                       </div>
   
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Last Name</label>
+                              <input type="text" name="last_name" class="form-control" value="{{ Request::get('last_name') }}" placeholder="Last Name">
+                          </div>
+                       </div>
+  
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Email</label>
+                              <input type="text" name="email" class="form-control" value="{{ Request::get('email') }}" placeholder="Email">
+                          </div>
+                       </div>
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Phone</label>
+                              <input type="text" name="phone" class="form-control" value="{{ Request::get('phone') }}" placeholder="Phone">
+                          </div>
+                       </div>
+  
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Country</label>
+                              <input type="text" name="country" class="form-control" value="{{ Request::get('country') }}" placeholder="Country">
+                          </div>
+                       </div>
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">State</label>
+                              <input type="text" name="state" class="form-control" value="{{ Request::get('state') }}" placeholder="State">
+                          </div>
+                       </div>
+
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">City</label>
+                              <input type="text" name="city" class="form-control" value="{{ Request::get('city') }}" placeholder=" City">
+                          </div>
+                       </div>
+
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Discount Code</label>
+                              <input type="text" name="discount_code" class="form-control" value="{{ Request::get('discount_code') }}" placeholder=" Discount Code">
+                          </div>
+                       </div>
+
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Pyment Method</label>
+                              <input type="text" name="payment_method" class="form-control" value="{{ Request::get('payment_method') }}" placeholder="Payment Method">
+                          </div>
+                       </div>
+
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">Form Date</label>
+                              <input type="date" name="form_date" class="form-control" value="{{ Request::get('form_date') }}" style="padding: 6px" placeholder="form_date">
+                          </div>
+                       </div>
+                       <div class="col-md-2">
+                          <div class="form-group">
+                             <label for="">To Date</label>
+                              <input type="date" name="to_date" class="form-control" value="{{ Request::get('to_date') }}" style="padding: 6px" placeholder="To Date">
+                          </div>
+                       </div>
+  
+                    </div>
+                    {{-- Search button start --}}
+                      <div class="row">
+                        <div class="col-md-12">
+                          <button class="btn btn-primary">Search</button>
+                          <a href="{{ url('admin/orders/list') }}" class="btn btn-danger">Reset</a>
+                        </div>
+                      </div>
+                      {{-- Search button end --}}
+                  </div>
+                 
+                </div>
+              </form>
+              {{-- Search form end --}}
+              
               <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">Order  List </h3>
+                  <h3 class="card-title">Order List </h3>
                 </div>
                 <!-- /.card-header -->
                 <div class="card-body p-0"  style="overflow: auto;">
@@ -46,19 +149,17 @@ Order Page
                         <th>Address</th>
                         <th>city</th>
                         <th>state</th>
-                    
                         <th>postcode</th>
                         <th>phone</th>
                         <th>email</th>
                         <th>Discount Code</th>
                         <th>Discount Amount</th>
+                        <th>Shipping Amount</th>
                         <th>Payment Method</th>
                         <th>Status</th>
-                        <th>Shipping Amount</th>
                         <th>Total Amount</th>
                         <th>Created Date</th>
                         <th>Action</th>
-                        
                       </tr>
                     </thead>
                     <tbody>
@@ -67,10 +168,10 @@ Order Page
                      
                       <tr>
                         <td>{{ $valu->id }}</td>
-                        <td>{{ $valu->first_name }}{{ $valu->last_name }}</td>
+                        <td>{{ $valu->first_name }} {{ $valu->last_name }}</td>
                         <td>{{ $valu->company_name }}</td>
                         <td>{{ $valu->country }}</td>
-                        <td>{{ $valu->address_one }}{{ $valu->address_two }}</td>
+                        <td>{{ $valu->address_one }} {{ $valu->address_two }}</td>
                         <td>{{ $valu->city }}</td>
                         <td>{{ $valu->state }}</td>
                         <td>{{ $valu->postcode }}</td>
